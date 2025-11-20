cff13aa414298118e0b23e3d3b2bf98f181164a6
박현우 <hyuns@hyuns.dev>
2025-11-20 13:16:44 +0900

    feat(db): Integrate Prisma ORM, define initial database schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-export * from '@prisma/client';
+import { PrismaClient } from './generated/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -11,3 +9,5 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export * from './generated/client';
