37508ea33b1768784beb669b2d866b941a525a94
박현우 <hyuns@hyuns.dev>
2025-11-25 15:55:47 +0900

    fix(api): UserModule Export에 Mapper 추가

--- apps/api/src/modules/user/user.module.ts
+++ apps/api/src/modules/user/user.module.ts
@@ -5,12 +5,13 @@ import { UserRepository } from './database/user.repository';
 import { GetUserHttpController } from './queries/get-user/get-user.http.controller';
 import { GetUserQueryHandler } from './queries/get-user/get-user.query';
 import { USER_REPOSITORY } from './user.di-tokens';
+import { UserDtoMapper } from './user.dto-mapper';
 import { UserMapper } from './user.mapper';
 
 const httpControllers = [GetUserHttpController];
 const commandHandlers: Provider[] = [];
 const queryHandlers: Provider[] = [GetUserQueryHandler];
-const mappers: Provider[] = [];
+const mappers: Provider[] = [UserMapper, UserDtoMapper];
 const repositories: Provider[] = [
   {
     provide: USER_REPOSITORY,
@@ -22,6 +23,6 @@ const repositories: Provider[] = [
   imports: [CqrsModule],
   providers: [Logger, ...commandHandlers, ...queryHandlers, ...mappers, ...repositories],
   controllers: [...httpControllers],
-  exports: [UserRepository, UserMapper],
+  exports: [UserRepository, UserMapper, UserDtoMapper],
 })
 export class UserModule {}
