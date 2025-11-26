63d72e5d778a66614e88394b7f8f291653b975cd
박현우 <hyuns@hyuns.dev>
2025-11-26 18:17:56 +0900

    feat(api): trust proxy 설정 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 4000);
+
+  // 프록시 서버 뒤에 있을 때 클라이언트 IP 얻기 위해 설정
+  app.set('trust proxy', true);
 
   app.enableCors();
   await app.listen(port);
