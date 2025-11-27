834605e9af6091f14b2e0fad12ce5e17c5cdf8da
박현우 <hyuns@hyuns.dev>
2025-11-27 14:48:27 +0900

    feat(api): mapDomainErrorToResponse() 유틸 함수 추가

---
+++ apps/api/src/common/utils/map-error-to-response.ts
@@ -0,0 +1,34 @@
+import { ArgumentInvalidException, ConflictException, NotFoundException } from '@/libs/exceptions';
+
+export function mapDomainErrorToResponse(error: Error) {
+  if (error instanceof NotFoundException) {
+    return {
+      status: 404,
+      body: {
+        code: error.code,
+        status: 404,
+        message: error.message,
+      },
+    } as const;
+  } else if (error instanceof ConflictException) {
+    return {
+      status: 409,
+      body: {
+        code: error.code,
+        status: 409,
+        message: error.message,
+      },
+    } as const;
+  } else if (error instanceof ArgumentInvalidException) {
+    return {
+      status: 400,
+      body: {
+        code: error.code,
+        status: 400,
+        message: error.message,
+      },
+    } as const;
+  }
+
+  throw error;
+}
