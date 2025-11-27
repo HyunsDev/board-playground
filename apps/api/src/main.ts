0bab5f0b5d805b4e25f4f731cb728c95a51dd605
박현우 <hyuns@hyuns.dev>
2025-11-27 14:46:09 +0900

    feat(api): cookie-parser 기능 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +10,9 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 4000);
+  const cookieSecret = configService.get<string>('COOKIE_SECRET');
+
+  app.use(cookieParser(cookieSecret));
 
   // 프록시 서버 뒤에 있을 때 클라이언트 IP 얻기 위해 설정
   app.set('trust proxy', true);
