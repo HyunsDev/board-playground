7282d06559f2e84e0380ba7c03aeb63f2cebbb03
박현우 <hyuns@hyuns.dev>
2025-11-27 14:47:01 +0900

    feat(api): api env 변경에 따른 ENV 스키마 변경

--- apps/api/src/config/env.validation.ts
+++ apps/api/src/config/env.validation.ts
@@ -7,6 +7,8 @@ export const envSchema = z.object({
 
   JWT_ACCESS_SECRET: z.string().min(32),
   JWT_ACCESS_EXPIRATION_TIME: z.string().default('15m'),
+
+  COOKIE_SECRET: z.string().min(32),
 });
 
 export type EnvSchema = z.infer<typeof envSchema>;
