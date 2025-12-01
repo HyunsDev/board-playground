d0d09e68b4aa9e5201aa07dd4cdbdf76dce2166c
박현우 <hyuns@hyuns.dev>
2025-12-01 15:02:31 +0900

    feat(api): save 메소드의 반환 타입에 NotFoundError 추가

--- apps/api/src/shared/base/domain/base.repository.port.ts
+++ apps/api/src/shared/base/domain/base.repository.port.ts
@@ -2,7 +2,7 @@ import { DomainResult } from '../../types/result.type';
 import { ConflictError, NotFoundError } from '../error/common.domain-errors';
 
 export interface RepositoryPort<Entity> {
-  save(entity: Entity): Promise<DomainResult<Entity, ConflictError>>;
+  save(entity: Entity): Promise<DomainResult<Entity, ConflictError | NotFoundError>>;
   findOneById(id: string): Promise<Entity | null>;
   delete(entity: Entity): Promise<DomainResult<void, NotFoundError>>;
 }
