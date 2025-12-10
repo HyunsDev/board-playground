5bf0fd07186117ca4893da905486948d5f6aa842
박현우 <hyuns@hyuns.dev>
2025-12-10 19:12:44 +0900

    docs: HandlerResult 주석 추가

--- packages/backend-common/src/types/handler-result.type.ts
+++ packages/backend-common/src/types/handler-result.type.ts
@@ -1,5 +1,8 @@
 import { SimplifyResult } from '@workspace/common';
 
+/**
+ * Handler의 execute 메서드의 반환 타입을 추출하는 유틸리티 타입
+ */
 export type HandlerResult<T extends { execute: (...args: any) => any }> = SimplifyResult<
   Awaited<ReturnType<T['execute']>>
 >;
