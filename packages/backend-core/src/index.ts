cdec58e4b0ad79d40fae0b950b5faa07283b7a43
박현우 <hyuns@hyuns.dev>
2025-12-12 01:41:47 +0900

    refactor(be-core): logger -> logging 이름 변경

--- packages/backend-core/src/index.ts
+++ packages/backend-core/src/index.ts
@@ -3,5 +3,5 @@ export * from './common';
 export * from './context';
 export * from './database';
 export * from './event-bus';
-export * from './logger';
+export * from './logging';
 export * from './config';
