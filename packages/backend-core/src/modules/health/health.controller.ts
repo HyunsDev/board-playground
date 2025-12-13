c6e64bf9b5b1e7b0477f55d8276826ec68e800b2
박현우 <hyuns@hyuns.dev>
2025-12-13 11:54:47 +0900

    feat(be-core): health 모듈 public 추가, /health/live 추가

--- packages/backend-core/src/modules/health/health.controller.ts
+++ packages/backend-core/src/modules/health/health.controller.ts
@@ -4,6 +4,7 @@ import { HealthCheck, HealthCheckService, PrismaHealthIndicator } from '@nestjs/
 
 import { PrismaService } from '../database';
 import { HEALTH_OPTIONS, HealthModuleOptions } from './health.interface';
+import { Public } from '../security';
 
 @Controller('health')
 export class HealthController {
@@ -15,32 +16,23 @@ export class HealthController {
     private options: HealthModuleOptions,
   ) {}
 
+  @Public()
   @Get()
-  @HealthCheck()
-  check() {
-    const indicators = [];
-
-    // 옵션에 따라 DB 체크 추가
-    if (this.options.checkDatabase) {
-      indicators.push(() => this.prismaHealth.pingCheck('database', this.prismaService));
-    }
-
-    // 필요하다면 메모리 체크 등 기본 체크 추가 가능
-    // indicators.push(() => this.memoryHealth.checkHeap('memory_heap', 150 * 1024 * 1024));
-
-    return this.health.check(indicators);
-  }
-
-  @Get('ready')
   @HealthCheck()
   readiness() {
     const indicators = [];
 
-    // 옵션에 따라 DB 체크 추가
     if (this.options.checkDatabase) {
       indicators.push(() => this.prismaHealth.pingCheck('database', this.prismaService));
     }
 
     return this.health.check(indicators);
+  }
+
+  @Public()
+  @Get('live')
+  @HealthCheck()
+  check() {
+    return this.health.check([]);
   }
 }
