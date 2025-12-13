fb646913138bff13c0df94959b5273536bf66a4e
박현우 <hyuns@hyuns.dev>
2025-12-13 14:17:00 +0900

    feat(be-core): CoreConfig 모듈 기본 load 삭제, 명시적 extraLoad 필요

--- packages/backend-core/src/modules/config/core-config.module.ts
+++ packages/backend-core/src/modules/config/core-config.module.ts
@@ -1,26 +1,18 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigFactory, ConfigModule } from '@nestjs/config';
 
-import { redisConfig } from './configs';
-import { accessTokenConfig } from './configs/access-token.config';
 import { coreConfig } from './configs/core.config';
-import { httpConfig } from './configs/http.config';
-import { prismaConfig } from './configs/prisma.config';
 
 export interface CoreConfigOptions {
-  load?: ConfigFactory[];
   extraLoad?: ConfigFactory[];
 }
 
 @Module({})
 export class CoreConfigModule {
   static forRoot(options: CoreConfigOptions = {}): DynamicModule {
-    const {
-      load = [coreConfig, httpConfig, prismaConfig, accessTokenConfig, redisConfig],
-      extraLoad = [],
-    } = options;
+    const { extraLoad = [] } = options;
 
-    const finalLoad = [...load, ...extraLoad];
+    const finalLoad = [coreConfig, ...extraLoad];
 
     return {
       module: CoreConfigModule,
