a50c2f28ce9a0d374d8bb311b52234b8b8917a0a
박현우 <hyuns@hyuns.dev>
2025-12-17 10:25:22 +0900

    feat(infra): StorageStack 추가

--- apps/infra/bin/infra.ts
+++ apps/infra/bin/infra.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib/core';
 
 import { InfraStack } from '../lib/infra-stack';
+import { StorageStack } from '../lib/storage-stack';
 
 const app = new cdk.App();
 new InfraStack(app, 'InfraStack', {
@@ -15,4 +17,11 @@ new InfraStack(app, 'InfraStack', {
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
+new StorageStack(app, 'StorageStack-Dev', {
+  stage: 'dev',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'ap-northeast-2', // 서울 리전
+  },
 });
