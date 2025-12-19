5014d0f586fb654e36eafb2dd0cf599bb84da00f
박현우 <hyuns@hyuns.dev>
2025-12-19 12:48:22 +0900

    refactor(be-core): DatabaseModule에 DatabaseURL 명시로 변경

--- packages/backend-core/src/modules/database/database.module.ts
+++ packages/backend-core/src/modules/database/database.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 
 import { PrismaService } from './prisma.service';
 
-@Global() // Database/Transaction은 전역에서 쓰이는 경우가 많음
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
