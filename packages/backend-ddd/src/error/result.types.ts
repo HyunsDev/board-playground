89144a08447a4687be77272b627c66dd49abef90
박현우 <hyuns@hyuns.dev>
2025-12-27 14:35:13 +0900

    refactor(ddd): DomainResult err 기본값 삭제

--- packages/backend-ddd/src/error/result.types.ts
+++ packages/backend-ddd/src/error/result.types.ts
@@ -2,10 +2,8 @@ import { Result, ResultAsync } from 'neverthrow';
 
 import { DomainError } from './base.domain-errors';
 
-export type DomainResult<T, E extends DomainError = DomainError> = Result<T, E>;
+export type DomainResult<T, E extends DomainError> = Result<T, E>;
 
-export type DomainResultAsync<T, E extends DomainError = DomainError> = ResultAsync<T, E>;
+export type DomainResultAsync<T, E extends DomainError> = ResultAsync<T, E>;
 
-export type PromiseDomainResult<T, E extends DomainError = DomainError> = Promise<
-  DomainResult<T, E>
->;
+export type PromiseDomainResult<T, E extends DomainError> = Promise<DomainResult<T, E>>;
